3f6a5fd0d4cc853b16d11ba4ee02dd770cf9db89
bartangutan <19bartosz11@gmail.com>
2026-01-26 14:01:20 +0100

    wiadomosc

---
+++ app/src/main/java/com/example/testtaknie/MainActivity.java
@@ -0,0 +1,14 @@
+package com.example.testtaknie;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
